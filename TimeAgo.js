8ab12a3e29989acaa2adff4baf377d3e868f841f
Artem Muravev <a.muraviev@rbr.moscow>
2017-08-01 08:57:14 +0300

    Make it as autoupdating time string with appearing like '10.25'/'yesterday'/'last week'/'02.02.2016' (configuring still through moment.locale)

--- TimeAgo.js
+++ TimeAgo.js
@@ -1,51 +1,55 @@
-var React = require('react')
-var ReactNative = require('react-native');
-var moment = require('moment');
-var TimerMixin = require('react-timer-mixin');
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
+import moment from 'moment';
+import TimerMixin from 'react-timer-mixin';
+import reactMixin from 'react-mixin';
 
-var { PropTypes } = React;
-var { Text } = ReactNative;
+class TimeString extends React.PureComponent {
 
-var TimeAgo = React.createClass({
-  mixins: [TimerMixin],
-  propTypes: {
-    time: React.PropTypes.oneOfType([
-      React.PropTypes.string,
-      React.PropTypes.number,
-      React.PropTypes.array,
-      React.PropTypes.instanceOf(Date)
-    ]).isRequired,
-    interval: PropTypes.number,
-    hideAgo: PropTypes.bool
-  },
-
-  getDefaultProps() {
-    return {
-      hideAgo: false,
-      interval: 60000
-    }
-  },
+  constructor(props) {
+    super(props);
+    this.update = this.update.bind(this);
+  }
 
   componentDidMount() {
-    var {interval} = this.props;
+    const { interval } = this.props;
     this.setInterval(this.update, interval);
-  },
+  }
 
   componentWillUnmount() {
     this.clearInterval(this.update);
-  },
+  }
 
   // We're using this method because of a weird bug
   // where autobinding doesn't seem to work w/ straight this.forceUpdate
   update() {
     this.forceUpdate();
-  },
+  }
 
   render() {
     return (
-      <Text {...this.props}>{moment(this.props.time).fromNow(this.props.hideAgo)}</Text>
+      <Text {...this.props}>{moment(this.props.time).calendar()}</Text>
     );
   }
-});
+}
 
-module.exports = TimeAgo;
+TimeString.propTypes = {
+  time: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.number,
+    React.PropTypes.array,
+    React.PropTypes.instanceOf(Date)
+  ]).isRequired,
+  interval: PropTypes.number,
+  hideAgo: PropTypes.bool
+};
+
+TimeString.defaultProps = {
+  hideAgo: false,
+  interval: 60000
+};
+
+reactMixin(TimeString.prototype, TimerMixin);
+
+module.exports = TimeString;
